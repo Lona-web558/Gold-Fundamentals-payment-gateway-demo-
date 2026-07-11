451586511b28f9d2ae0c551d054b5f092d17afff
Lona Smith <71089848+Lona-web558@users.noreply.github.com>
2026-07-11 13:03:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use("/v1", paymentRoutes);
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "public", "Gateway.html"));
 });
 const PORT = process.env.PORT || 8080;
 
